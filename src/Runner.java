b549f7835111dd574092542e85b005a83a76f8e9
Sam Balthazard <sam.balthazard2014@gmail.com>
2018-05-30 16:21:54 +1200

    Fixed input layer mismatch with image size in Runner.

--- src/Runner.java
+++ src/Runner.java
@@ -3,12 +3,16 @@ public class Runner {
 	
 	public static void main(String[] args) {
 		
-		int[] leafIDBrainLayerSizes = {10800 , 35 , 20 , 3};
+		final int IMAGE_WIDTH = 160;
+		final int IMAGE_HEIGHT = 90;
+		final int NUM_PIXEL_CHARACTERISTICS = 3; // R, G, and B
+		
+		int[] leafIDBrainLayerSizes = {IMAGE_WIDTH * IMAGE_HEIGHT * NUM_PIXEL_CHARACTERISTICS , 35 , 20 , 3};
 		try {
 			
 			ImageBrain leafIDBrain = new ImageBrain(leafIDBrainLayerSizes);
 			
-			String[] leafIDInformation = {"images/leafData.txt" , "images/" , "80" , "45" , "1"};
+			String[] leafIDInformation = {"images/leafData.txt" , "images/" , IMAGE_WIDTH + "" , IMAGE_HEIGHT + "" , "1"};
 			leafIDBrain.loadData(leafIDInformation);
 			leafIDBrain.train();
 			
